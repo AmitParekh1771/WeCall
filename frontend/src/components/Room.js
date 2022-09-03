5fb5c01e181e63d21368749478fff87ac6eb3b6d
Shilp <shilpkishor27@gmail.com>
2022-09-04 02:44:00 +0530

    added non stylish whatsapp and email sharing links

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,18 +1,33 @@
+import { Button } from '@material-ui/core'
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
-
+import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const Room = () => {
+  const navigate = useNavigate()
 
-  const navigate = useNavigate();
+  let { rid } = useParams()
 
-  function handleEndCall(){
-    console.log("clicked");
+  function handleEndCall() {
+    console.log('clicked')
     navigate(`/join`)
   }
 
+  let msg = 'whatsapp://send?text=Join the video call using this room id '
+  msg = msg + { rid }.rid
+  let emailmsg =
+    'mailto:?subject=Join the video call&body=Join the video call using this room id '
+  emailmsg = emailmsg + { rid }.rid
   return (
     <>
+      <button>
+        <a href={msg}>Share Via WhatsApp</a>
+      </button>
+      <button>
+        <a href={emailmsg} target='_blank'>
+          Share via Email
+        </a>
+      </button>
       <div className='section room-section'>
         <div className='outer-container-1360'>
           <div className='videos-container'>
@@ -37,7 +52,9 @@ const Room = () => {
               <div className='fa-solid'></div>
             </div>
             <div className='end-btn'>
-              <div onClick = {handleEndCall} className='fa-solid rotate'></div>
+              <div onClick={handleEndCall} className='fa-solid rotate'>
+                
+              </div>
             </div>
           </div>
         </div>
