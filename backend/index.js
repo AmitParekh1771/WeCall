18063748b9b174e4c33cf91925dca0d06f102267
Amit Parekh <parekhamit04@gmail.com>
2022-09-05 00:03:05 +0530

    video calling feature implemented

--- backend/index.js
+++ backend/index.js
@@ -3,87 +3,92 @@ const WebSocketServer = require('ws').Server;
 
 const wss = new WebSocketServer({ port: 4000 })
 
-let users = []
+let rooms = []
 
 function sendData(data, connection) {
     connection.send(JSON.stringify(data));
 }
  
-function findUser(username) {
-    return users.find(user => user.username == username);
+function findRoom(roomId) {
+    return rooms.find(room => room.roomId == roomId);
 }
  
 wss.on('connection', (ws, req) => {
+    console.log(rooms);
     ws.on('message', (message) => {
         const data = JSON.parse(message);
  
-        const user = findUser(data.username);
+        const room = findRoom(data.roomId);
  
         switch(data.type) {
-            case "store_user":
-                if (user) return;
+            case "room":
+                if(room) return;
  
-                const newUser = {
-                    conn: ws,
-                    username: data.username
+                const newRoom = {
+                    connection: ws,
+                    roomId: data.roomId
                 }
-                users.push(newUser);
-                console.log(newUser.username);
+                rooms.push(newRoom);
+                console.log(newRoom.roomId);
                 break;
 
-            case "store_offer":
-                if (!user) return;
-                user.offer = data.offer;
+            case "set_offer":
+                if(!room) return;
+                room.offer = data.offer;
                 break;
             
-            case "store_candidate":
-                if (!user) return;
-
-                if (!user.candidates) user.candidates = [];
+            case "get_offer":
+                if(!room) return;
                 
-                user.candidates.push(data.candidate);
+                sendData({
+                    type: 'offer',
+                    offer: room.offer
+                }, ws);
                 break;
-
+            
             case "send_answer":
-                if (!user) return;
- 
-                sendData({
-                    type: "answer",
-                    answer: data.answer
-                }, user.conn);
-                break;
-
-            case "send_candidate":
-                if (!user) return;
- 
-                sendData({
-                    type: "candidate",
-                    candidate: data.candidate
-                }, user.conn);
-                break;
-
-            case "join_call":
-                if (!user) return;
-
-                sendData({
-                    type: "offer",
-                    offer: user.offer
-                }, ws)
+                if(!room) return;
                 
-                user.candidates.forEach(candidate => {
-                    sendData({
-                        type: "candidate",
-                        candidate: candidate
-                    }, ws)
-                })
+                sendData({
+                    type: 'answer',
+                    answer: data.answer
+                }, room.connection);
+                break;
+
+            case "set_offer_candidate":
+                if (!room) return;
+                room.offerCandidate = data.offerCandidate;
+                break;
+            
+            case "get_offer_candidate":
+                if (!room) return;
+
+                sendData({
+                    type: 'offer_candidate',
+                    offerCandidate: room.offerCandidate
+                }, ws);
+                break;
+            
+            case "set_answer_candidate":
+                if (!room) return;
+                room.answerCandidate = data.answerCandidate;
+                break;
+
+            case "send_answer_candidate":
+                if (!room) return;
+
+                sendData({
+                    type: 'answer_candidate',
+                    answerCandidate: data.answerCandidate
+                }, room.connection);
                 break;
         }
     })
  
     ws.on('close', (reason, description) => {
-        const index = users.findIndex(user => user.conn == ws);
+        const index = rooms.findIndex(room => room.connection == ws);
 
-        users.splice(index, 1);
+        rooms.splice(index, 1);
     })
 })
  
--- ui/wecall.js
+++ ui/wecall.js
@@ -1,3 +1,25 @@
+const servers = {
+    iceServers: [
+        {
+            urls: [
+                'stun:stun1.l.google.com:19302', 
+                'stun:stun2.l.google.com:19302'
+            ]
+        }
+    ],
+    iceCandidatePoolSize: 10
+}
+
+let pc = new RTCPeerConnection(servers);
+
+let ws = new WebSocket('ws://localhost:4000');
+
+
+
+let localStream = new MediaStream();
+let remoteStream = new MediaStream();
+
+
 /**
  * function to start video stream
  * 
@@ -5,10 +27,15 @@
  */
 async function startStream(el) {
     if(navigator.mediaDevices.getUserMedia) {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
 
-        el.srcObject = stream;
-        console.log(stream.getTracks())
+        el.srcObject = localStream;
+
+        localStream.getTracks().forEach(track => {
+            pc.addTrack(track);
+        });
+        console.log(localStream.getTracks())
+
     }
 }
 
@@ -27,46 +54,130 @@ function stopStream(el) {
     }
 
     el.srcObject = null;
+
 }
 
 
-let videoEle = document.getElementById('videoElement');
-// startStream(videoEle);
+let localVideo = document.getElementById('localVideo');
+let remoteVideo = document.getElementById('remoteVideo');
+startStream(localVideo);
 
-function toggleStream() {
-    if(videoEle.srcObject) {
-        stopStream(videoEle);
-    }
-    else {
-        startStream(videoEle);
-    }
-}
-
-const servers = {
-    iceServers: [
-        {
-            urls: [
-                'stun:stun1.l.google.com:19302', 
-                'stun:stun2.l.google.com:19302'
-            ]
-        }
-    ],
-    iceCandidatePoolSize: 10
-}
-
-let pc = new RTCPeerConnection(servers);
-
-let ws = new WebSocket('ws://localhost:4000/123')
-
-ws.addEventListener('open', (ev) => {
-    console.log('client connected');
-    let msg = JSON.stringify({
-        msg: 'hello'
+function toggleStream(type) {
+    // if(localVideo.srcObject) {
+    //     stopStream(localVideo);
+    // }
+    // else {
+    //     startStream(localVideo);
+    // }
+    localStream.getTracks().forEach(track => {
+        if(track.kind == type)
+            track.enabled = !track.enabled;
     })
-    ws.send(msg)
+}
+        
+pc.addEventListener('track', (ev) => {
+    remoteStream.addTrack(ev.track);
+    remoteVideo.srcObject = remoteStream;
+    console.log(ev)
+})
+
+
+function sendData(data) {
+    ws.send(JSON.stringify(data));
+}
+
+async function createMeet() {
+    sendData({
+        type: 'room',
+        roomId: 'AMIT'
+    });
+
+    pc.onicecandidate = event => {
+        console.log('setting offer candidate');
+        event.candidate && sendData({ type: 'set_offer_candidate', roomId: 'AMIT', offerCandidate: event.candidate.toJSON() })
+    }
+
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
+
+    const offer = {
+        sdp: offerDescription.sdp,
+        type: offerDescription.type
+    }
+
+    sendData({
+        type: 'set_offer',
+        roomId: 'AMIT',
+        offer
+    })
+    console.log('after setting offer candidate', pc)
+}
+
+ws.addEventListener('open', ev => {
+    console.log(ev);
 })
 
 ws.addEventListener('message', (ev) => {
-    console.log(ev.data)
+    const data = JSON.parse(ev.data);
+    
+    if(data.type == 'answer_candidate') {
+        const candidate = new RTCIceCandidate(data.answerCandidate);
+        pc.addIceCandidate(candidate);
+    }
+    
+    if(data.type == 'offer_candidate') {
+        const candidate = new RTCIceCandidate(data.offerCandidate);
+        pc.addIceCandidate(candidate);
+    }
+
+    if(!pc.currentRemoteDescription && data.type == 'answer') {
+        const answerDescription = new RTCSessionDescription(data.answer);
+        pc.setRemoteDescription(answerDescription);
+    }
+
+    if(!pc.currentRemoteDescription && data.type == 'offer') {
+        const offerDescription = new RTCSessionDescription(data.offer);
+        pc.setRemoteDescription(offerDescription);
+    }
+    
 })
 
+async function joinMeet() {
+    sendData({
+        type: 'get_offer',
+        roomId: 'AMIT'
+    })
+
+    
+    if(pc.signalingState != 'have-remote-offer' && pc.signalingState != 'have-local-pranswer') return;
+    console.log('before setting remote candidate', pc)
+
+
+    sendData({
+        type: 'get_offer_candidate',
+        roomId: 'AMIT'
+    })
+
+    pc.onicecandidate = event => {
+        console.log('setting answer candidate');
+        if(!event.candidate) return;
+        sendData({ type: 'set_answer_candidate', roomId: 'AMIT', answerCandidate: event.candidate.toJSON() })
+        sendData({ type: 'send_answer_candidate', roomId: 'AMIT', answerCandidate: event.candidate.toJSON() })
+    }
+
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(answerDescription);
+
+    const answer = {
+        sdp: answerDescription.sdp,
+        type: answerDescription.type
+    }
+
+    sendData({
+        type: 'send_answer',
+        roomId: 'AMIT',
+        answer
+    })
+
+    console.log('after setting remote candidate', pc)
+}
